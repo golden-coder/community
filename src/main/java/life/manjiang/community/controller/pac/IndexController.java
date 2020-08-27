47542e2fda5dc723a574d1198b635d39f45931d1
libing <yasoc2014519019@163.com>
2020-08-27 16:02:25 +0800

    init repo

---
+++ src/main/java/life/manjiang/community/controller/pac/IndexController.java
@@ -0,0 +1,16 @@
+package life.manjiang.community.controller.pac;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+
+    }
+}
